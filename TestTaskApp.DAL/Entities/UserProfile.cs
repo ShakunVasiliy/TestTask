b630a0c8d1faf80da1d669209c8b1804ca0da6be
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-07-29 21:21:12 +0300

    Finish

--- TestTaskApp.DAL/Entities/UserProfile.cs
+++ TestTaskApp.DAL/Entities/UserProfile.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace TestTaskApp.DAL.Entities
 {
     public class UserProfile
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Email { get; set; }
+
         public string Title { get; set; }
         public string Salutation { get; set; }
-        
-        
+        public DateTime DateOfBirth { get; set; }
+        public string Bio { get; set; }
+        public string ImagePath { get; set; }
+
         public int? ManagerId { get; set; }
         public virtual UserProfile Manager { get; set; }
 
         public virtual ICollection<UserProfile> UserProfiles { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
-        public string Bio { get; set; }
-        public string ImagePath { get; set; }
 
         public UserProfile()
         {
